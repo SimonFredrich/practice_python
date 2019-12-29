c38ad2fb96403567ffaffbd7436f249f0a9c07ab
SimonFredrich <21simplex02@gmail.com>
2019-12-29 15:05:21 +0100

    optimize code structure

--- 10_list_overlap_comprehensions.py
+++ 10_list_overlap_comprehensions.py
@@ -1,30 +1,23 @@
+import random
+import csv
+
 emptyList = []
 fullList = []
-rounds = 0
 
 def listComprehension():
-    import random
-
     a = list(random.sample(range(80), random.randint(10, 20)))
     b = list(random.sample(range(80), random.randint(10, 20)))
 
     checkList = [item for item in a if item in b]
 
-
-    def counter():
-        if len(checkList) == 0:
-            emptyList.append(checkList)
-        else:
-            fullList.append(checkList)
-        
-    counter()
-
+    if len(checkList) == 0:
+        emptyList.append(checkList)
+    else:
+        fullList.append(checkList)
 
 def writeCVS():
-    import csv
 
-    global rounds
-
+    rounds = 0
     while rounds < 1000:
         listComprehension()
         rounds += 1
@@ -39,7 +32,7 @@ def writeCVS():
     with open("Excel/comprehension.csv", "a") as csv.file:
         writer = csv.writer(csv.file)
         writer.writerow([rounds, len(fullList), len(emptyList)])
-'''
+    '''
 writeCVS()
 
 print(emptyList)
